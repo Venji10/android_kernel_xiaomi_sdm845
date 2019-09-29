d7cfe48571dc39df6da48523d64f1f003df78af0
Venji10 <bennisteinir@gmail.com>
2019-09-29 11:21:37 +0200

    upstream gpu drivers

--- drivers/gpu/drm/msm/msm_gem_vma.c
+++ drivers/gpu/drm/msm/msm_gem_vma.c
@@ -1,363 +1,174 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (C) 2016 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published by
- * the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "msm_drv.h"
 #include "msm_gem.h"
 #include "msm_mmu.h"
 
-/* SDE address space operations */
-static void smmu_aspace_unmap_vma(struct msm_gem_address_space *aspace,
-		struct msm_gem_vma *vma, struct sg_table *sgt,
-		void *priv)
+static void
+msm_gem_address_space_destroy(struct kref *kref)
 {
-	struct dma_buf *buf = priv;
+	struct msm_gem_address_space *aspace = container_of(kref,
+			struct msm_gem_address_space, kref);
 
-	if (buf)
-		aspace->mmu->funcs->unmap_dma_buf(aspace->mmu,
-			sgt, buf, DMA_BIDIRECTIONAL);
-	else
-		aspace->mmu->funcs->unmap_sg(aspace->mmu, sgt,
-			DMA_BIDIRECTIONAL);
-
-	vma->iova = 0;
+	drm_mm_takedown(&aspace->mm);
+	if (aspace->mmu)
+		aspace->mmu->funcs->destroy(aspace->mmu);
+	kfree(aspace);
 }
 
 
-static int smmu_aspace_map_vma(struct msm_gem_address_space *aspace,
-		struct msm_gem_vma *vma, struct sg_table *sgt,
-		void *priv, unsigned int flags)
+void msm_gem_address_space_put(struct msm_gem_address_space *aspace)
 {
-	struct dma_buf *buf = priv;
-	int ret;
+	if (aspace)
+		kref_put(&aspace->kref, msm_gem_address_space_destroy);
+}
 
-	if (!aspace || !aspace->domain_attached)
+/* Actually unmap memory for the vma */
+void msm_gem_purge_vma(struct msm_gem_address_space *aspace,
+		struct msm_gem_vma *vma)
+{
+	unsigned size = vma->node.size << PAGE_SHIFT;
+
+	/* Print a message if we try to purge a vma in use */
+	if (WARN_ON(vma->inuse > 0))
+		return;
+
+	/* Don't do anything if the memory isn't mapped */
+	if (!vma->mapped)
+		return;
+
+	if (aspace->mmu)
+		aspace->mmu->funcs->unmap(aspace->mmu, vma->iova, size);
+
+	vma->mapped = false;
+}
+
+/* Remove reference counts for the mapping */
+void msm_gem_unmap_vma(struct msm_gem_address_space *aspace,
+		struct msm_gem_vma *vma)
+{
+	if (!WARN_ON(!vma->iova))
+		vma->inuse--;
+}
+
+int
+msm_gem_map_vma(struct msm_gem_address_space *aspace,
+		struct msm_gem_vma *vma, int prot,
+		struct sg_table *sgt, int npages)
+{
+	unsigned size = npages << PAGE_SHIFT;
+	int ret = 0;
+
+	if (WARN_ON(!vma->iova))
 		return -EINVAL;
 
-	if (buf)
-		ret = aspace->mmu->funcs->map_dma_buf(aspace->mmu, sgt, buf,
-			DMA_BIDIRECTIONAL, flags);
-	else
-		ret = aspace->mmu->funcs->map_sg(aspace->mmu, sgt,
-			DMA_BIDIRECTIONAL);
+	/* Increase the usage counter */
+	vma->inuse++;
 
-	if (!ret)
-		vma->iova = sg_dma_address(sgt->sgl);
+	if (vma->mapped)
+		return 0;
+
+	vma->mapped = true;
+
+	if (aspace && aspace->mmu)
+		ret = aspace->mmu->funcs->map(aspace->mmu, vma->iova, sgt,
+				size, prot);
+
+	if (ret)
+		vma->mapped = false;
 
 	return ret;
 }
 
-static void smmu_aspace_destroy(struct msm_gem_address_space *aspace)
+/* Close an iova.  Warn if it is still in use */
+void msm_gem_close_vma(struct msm_gem_address_space *aspace,
+		struct msm_gem_vma *vma)
 {
-	aspace->mmu->funcs->destroy(aspace->mmu);
-}
-
-static void smmu_aspace_add_to_active(
-		struct msm_gem_address_space *aspace,
-		struct msm_gem_object *msm_obj)
-{
-	WARN_ON(!mutex_is_locked(&aspace->dev->struct_mutex));
-	list_move_tail(&msm_obj->iova_list, &aspace->active_list);
-	msm_obj->in_active_list = true;
-}
-
-static void smmu_aspace_remove_from_active(
-		struct msm_gem_address_space *aspace,
-		struct msm_gem_object *obj)
-{
-	struct msm_gem_object *msm_obj, *next;
-
-	WARN_ON(!mutex_is_locked(&aspace->dev->struct_mutex));
-
-	list_for_each_entry_safe(msm_obj, next, &aspace->active_list,
-			iova_list) {
-		if (msm_obj == obj) {
-			msm_obj->in_active_list = false;
-			list_del(&msm_obj->iova_list);
-			break;
-		}
-	}
-}
-
-static int smmu_aspace_register_cb(
-		struct msm_gem_address_space *aspace,
-		void (*cb)(void *, bool),
-		void *cb_data)
-{
-	struct aspace_client *aclient = NULL;
-	struct aspace_client *temp;
-
-	if (!aspace)
-		return -EINVAL;
-
-	if (!aspace->domain_attached)
-		return -EACCES;
-
-	aclient = kzalloc(sizeof(*aclient), GFP_KERNEL);
-	if (!aclient)
-		return -ENOMEM;
-
-	aclient->cb = cb;
-	aclient->cb_data = cb_data;
-	INIT_LIST_HEAD(&aclient->list);
-
-	/* check if callback is already registered */
-	mutex_lock(&aspace->dev->struct_mutex);
-	list_for_each_entry(temp, &aspace->clients, list) {
-		if ((temp->cb == aclient->cb) &&
-			(temp->cb_data == aclient->cb_data)) {
-			kfree(aclient);
-			mutex_unlock(&aspace->dev->struct_mutex);
-			return -EEXIST;
-		}
-	}
-
-	list_move_tail(&aclient->list, &aspace->clients);
-	mutex_unlock(&aspace->dev->struct_mutex);
-
-	return 0;
-}
-
-static int smmu_aspace_unregister_cb(
-		struct msm_gem_address_space *aspace,
-		void (*cb)(void *, bool),
-		void *cb_data)
-{
-	struct aspace_client *aclient = NULL;
-	int rc = -ENOENT;
-
-	if (!aspace || !cb)
-		return -EINVAL;
-
-	mutex_lock(&aspace->dev->struct_mutex);
-	list_for_each_entry(aclient, &aspace->clients, list) {
-		if ((aclient->cb == cb) &&
-			(aclient->cb_data == cb_data)) {
-			list_del(&aclient->list);
-			kfree(aclient);
-			rc = 0;
-			break;
-		}
-	}
-	mutex_unlock(&aspace->dev->struct_mutex);
-
-	return rc;
-}
-
-
-static const struct msm_gem_aspace_ops smmu_aspace_ops = {
-	.map = smmu_aspace_map_vma,
-	.unmap = smmu_aspace_unmap_vma,
-	.destroy = smmu_aspace_destroy,
-	.add_to_active = smmu_aspace_add_to_active,
-	.remove_from_active = smmu_aspace_remove_from_active,
-	.register_cb = smmu_aspace_register_cb,
-	.unregister_cb = smmu_aspace_unregister_cb,
-};
-
-struct msm_gem_address_space *
-msm_gem_smmu_address_space_create(struct drm_device *dev, struct msm_mmu *mmu,
-		const char *name)
-{
-	struct msm_gem_address_space *aspace;
-
-	if (!mmu)
-		return ERR_PTR(-EINVAL);
-
-	aspace = kzalloc(sizeof(*aspace), GFP_KERNEL);
-	if (!aspace)
-		return ERR_PTR(-ENOMEM);
-
-	aspace->dev = dev;
-	aspace->name = name;
-	aspace->mmu = mmu;
-	aspace->ops = &smmu_aspace_ops;
-	INIT_LIST_HEAD(&aspace->active_list);
-	INIT_LIST_HEAD(&aspace->clients);
-
-	return aspace;
-}
-
-/* GPU address space operations */
-struct msm_iommu_aspace {
-	struct msm_gem_address_space base;
-	struct drm_mm mm;
-};
-
-#define to_iommu_aspace(aspace) \
-	((struct msm_iommu_aspace *) \
-	 container_of(aspace, struct msm_iommu_aspace, base))
-
-static void iommu_aspace_unmap_vma(struct msm_gem_address_space *aspace,
-		struct msm_gem_vma *vma, struct sg_table *sgt, void *priv)
-{
-	if (!vma->iova)
+	if (WARN_ON(vma->inuse > 0 || vma->mapped))
 		return;
 
-	if (aspace->mmu)
-		aspace->mmu->funcs->unmap(aspace->mmu, vma->iova, sgt);
-
-	drm_mm_remove_node(&vma->node);
+	spin_lock(&aspace->lock);
+	if (vma->iova)
+		drm_mm_remove_node(&vma->node);
+	spin_unlock(&aspace->lock);
 
 	vma->iova = 0;
+
+	msm_gem_address_space_put(aspace);
 }
 
-static int iommu_aspace_map_vma(struct msm_gem_address_space *aspace,
-		struct msm_gem_vma *vma, struct sg_table *sgt,
-		void *priv, unsigned int flags)
+/* Initialize a new vma and allocate an iova for it */
+int msm_gem_init_vma(struct msm_gem_address_space *aspace,
+		struct msm_gem_vma *vma, int npages)
 {
-	struct msm_iommu_aspace *local = to_iommu_aspace(aspace);
-	size_t size = 0;
-	struct scatterlist *sg;
-	int ret = 0, i;
+	int ret;
 
-	if (WARN_ON(drm_mm_node_allocated(&vma->node)))
-		return 0;
+	if (WARN_ON(vma->iova))
+		return -EBUSY;
 
-	for_each_sg(sgt->sgl, sg, sgt->nents, i)
-		size += sg->length + sg->offset;
+	spin_lock(&aspace->lock);
+	ret = drm_mm_insert_node(&aspace->mm, &vma->node, npages);
+	spin_unlock(&aspace->lock);
 
-	ret = drm_mm_insert_node(&local->mm, &vma->node, size >> PAGE_SHIFT,
-			0, DRM_MM_SEARCH_DEFAULT);
 	if (ret)
 		return ret;
 
 	vma->iova = vma->node.start << PAGE_SHIFT;
+	vma->mapped = false;
 
-	if (aspace->mmu)
-		ret = aspace->mmu->funcs->map(aspace->mmu, vma->iova,
-			sgt, IOMMU_READ | IOMMU_WRITE);
+	kref_get(&aspace->kref);
 
-	return ret;
+	return 0;
 }
 
-static void iommu_aspace_destroy(struct msm_gem_address_space *aspace)
-{
-	struct msm_iommu_aspace *local = to_iommu_aspace(aspace);
-
-	drm_mm_takedown(&local->mm);
-	aspace->mmu->funcs->destroy(aspace->mmu);
-}
-
-static const struct msm_gem_aspace_ops msm_iommu_aspace_ops = {
-	.map = iommu_aspace_map_vma,
-	.unmap = iommu_aspace_unmap_vma,
-	.destroy = iommu_aspace_destroy,
-};
-
-static struct msm_gem_address_space *
-msm_gem_address_space_new(struct msm_mmu *mmu, const char *name,
-		uint64_t start, uint64_t end)
-{
-	struct msm_iommu_aspace *local;
-
-	if (!mmu)
-		return ERR_PTR(-EINVAL);
-
-	local = kzalloc(sizeof(*local), GFP_KERNEL);
-	if (!local)
-		return ERR_PTR(-ENOMEM);
-
-	drm_mm_init(&local->mm, (start >> PAGE_SHIFT),
-		(end >> PAGE_SHIFT) - 1);
-
-	local->base.name = name;
-	local->base.mmu = mmu;
-	local->base.ops = &msm_iommu_aspace_ops;
-
-	return &local->base;
-}
-
-int msm_gem_map_vma(struct msm_gem_address_space *aspace,
-		struct msm_gem_vma *vma, struct sg_table *sgt,
-		void *priv, unsigned int flags)
-{
-	if (aspace && aspace->ops->map)
-		return aspace->ops->map(aspace, vma, sgt, priv, flags);
-
-	return -EINVAL;
-}
-
-void msm_gem_unmap_vma(struct msm_gem_address_space *aspace,
-		struct msm_gem_vma *vma, struct sg_table *sgt, void *priv)
-{
-	if (aspace && aspace->ops->unmap)
-		aspace->ops->unmap(aspace, vma, sgt, priv);
-}
 
 struct msm_gem_address_space *
 msm_gem_address_space_create(struct device *dev, struct iommu_domain *domain,
 		const char *name)
 {
-	struct msm_mmu *mmu = msm_iommu_new(dev, domain);
+	struct msm_gem_address_space *aspace;
+	u64 size = domain->geometry.aperture_end -
+		domain->geometry.aperture_start;
 
-	if (IS_ERR(mmu))
-		return (struct msm_gem_address_space *) mmu;
+	aspace = kzalloc(sizeof(*aspace), GFP_KERNEL);
+	if (!aspace)
+		return ERR_PTR(-ENOMEM);
 
-	return msm_gem_address_space_new(mmu, name,
-		domain->geometry.aperture_start,
-		domain->geometry.aperture_end);
+	spin_lock_init(&aspace->lock);
+	aspace->name = name;
+	aspace->mmu = msm_iommu_new(dev, domain);
+
+	drm_mm_init(&aspace->mm, (domain->geometry.aperture_start >> PAGE_SHIFT),
+		size >> PAGE_SHIFT);
+
+	kref_init(&aspace->kref);
+
+	return aspace;
 }
 
-void
-msm_gem_address_space_destroy(struct msm_gem_address_space *aspace)
+struct msm_gem_address_space *
+msm_gem_address_space_create_a2xx(struct device *dev, struct msm_gpu *gpu,
+		const char *name, uint64_t va_start, uint64_t va_end)
 {
-	if (aspace && aspace->ops->destroy)
-		aspace->ops->destroy(aspace);
+	struct msm_gem_address_space *aspace;
+	u64 size = va_end - va_start;
 
-	kfree(aspace);
+	aspace = kzalloc(sizeof(*aspace), GFP_KERNEL);
+	if (!aspace)
+		return ERR_PTR(-ENOMEM);
+
+	spin_lock_init(&aspace->lock);
+	aspace->name = name;
+	aspace->mmu = msm_gpummu_new(dev, gpu);
+
+	drm_mm_init(&aspace->mm, (va_start >> PAGE_SHIFT),
+		size >> PAGE_SHIFT);
+
+	kref_init(&aspace->kref);
+
+	return aspace;
 }
-
-void msm_gem_add_obj_to_aspace_active_list(
-		struct msm_gem_address_space *aspace,
-		struct drm_gem_object *obj)
-{
-	struct msm_gem_object *msm_obj = to_msm_bo(obj);
-
-	if (aspace && aspace->ops && aspace->ops->add_to_active)
-		aspace->ops->add_to_active(aspace, msm_obj);
-}
-
-void msm_gem_remove_obj_from_aspace_active_list(
-		struct msm_gem_address_space *aspace,
-		struct drm_gem_object *obj)
-{
-	struct msm_gem_object *msm_obj = to_msm_bo(obj);
-
-	if (aspace && aspace->ops && aspace->ops->remove_from_active)
-		aspace->ops->remove_from_active(aspace, msm_obj);
-}
-
-int msm_gem_address_space_register_cb(struct msm_gem_address_space *aspace,
-		void (*cb)(void *, bool),
-		void *cb_data)
-{
-	if (aspace && aspace->ops && aspace->ops->register_cb)
-		return aspace->ops->register_cb(aspace, cb, cb_data);
-
-	return -EINVAL;
-}
-
-int msm_gem_address_space_unregister_cb(struct msm_gem_address_space *aspace,
-		void (*cb)(void *, bool),
-		void *cb_data)
-{
-	if (aspace && aspace->ops && aspace->ops->unregister_cb)
-		return aspace->ops->unregister_cb(aspace, cb, cb_data);
-
-	return -EINVAL;
-}
-
--- drivers/gpu/drm/msm/msm_kms.h
+++ drivers/gpu/drm/msm/msm_kms.h
@@ -1,19 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2016-2018, The Linux Foundation. All rights reserved.
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published by
- * the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __MSM_KMS_H__
@@ -26,21 +15,6 @@
 
 #define MAX_PLANE	4
 
-/**
- * Device Private DRM Mode Flags
- * drm_mode->private_flags
- */
-/* Connector has interpreted seamless transition request as dynamic fps */
-#define MSM_MODE_FLAG_SEAMLESS_DYNAMIC_FPS	(1<<0)
-/* Transition to new mode requires a wait-for-vblank before the modeset */
-#define MSM_MODE_FLAG_VBLANK_PRE_MODESET	(1<<1)
-/* Request to switch the connector mode */
-#define MSM_MODE_FLAG_SEAMLESS_DMS			(1<<2)
-/* Request to switch the fps */
-#define MSM_MODE_FLAG_SEAMLESS_VRR			(1<<3)
-/* Request to switch the bit clk */
-#define MSM_MODE_FLAG_SEAMLESS_DYN_CLK			(1<<4)
-
 /* As there are different display controller blocks depending on the
  * snapdragon version, the kms support is split out and the appropriate
  * implementation is loaded at runtime.  The kms module is responsible
@@ -49,7 +23,6 @@
 struct msm_kms_funcs {
 	/* hw initialization: */
 	int (*hw_init)(struct msm_kms *kms);
-	int (*postinit)(struct msm_kms *kms);
 	/* irq handling: */
 	void (*irq_preinstall)(struct msm_kms *kms);
 	int (*irq_postinstall)(struct msm_kms *kms);
@@ -57,33 +30,86 @@ struct msm_kms_funcs {
 	irqreturn_t (*irq)(struct msm_kms *kms);
 	int (*enable_vblank)(struct msm_kms *kms, struct drm_crtc *crtc);
 	void (*disable_vblank)(struct msm_kms *kms, struct drm_crtc *crtc);
-	/* modeset, bracketing atomic_commit(): */
-	void (*prepare_fence)(struct msm_kms *kms,
-			struct drm_atomic_state *state);
-	void (*prepare_commit)(struct msm_kms *kms,
-			struct drm_atomic_state *state);
-	void (*commit)(struct msm_kms *kms, struct drm_atomic_state *state);
-	void (*complete_commit)(struct msm_kms *kms,
-			struct drm_atomic_state *state);
-	/* functions to wait for atomic commit completed on each CRTC */
-	void (*wait_for_crtc_commit_done)(struct msm_kms *kms,
-					struct drm_crtc *crtc);
-	/* function pointer to wait for pixel transfer to panel to complete*/
-	void (*wait_for_tx_complete)(struct msm_kms *kms,
-					struct drm_crtc *crtc);
+
+	/*
+	 * Atomic commit handling:
+	 *
+	 * Note that in the case of async commits, the funcs which take
+	 * a crtc_mask (ie. ->flush_commit(), and ->complete_commit())
+	 * might not be evenly balanced with ->prepare_commit(), however
+	 * each crtc that effected by a ->prepare_commit() (potentially
+	 * multiple times) will eventually (at end of vsync period) be
+	 * flushed and completed.
+	 *
+	 * This has some implications about tracking of cleanup state,
+	 * for example SMP blocks to release after commit completes.  Ie.
+	 * cleanup state should be also duplicated in the various
+	 * duplicate_state() methods, as the current cleanup state at
+	 * ->complete_commit() time may have accumulated cleanup work
+	 * from multiple commits.
+	 */
+
+	/**
+	 * Enable/disable power/clks needed for hw access done in other
+	 * commit related methods.
+	 *
+	 * If mdp4 is migrated to runpm, we could probably drop these
+	 * and use runpm directly.
+	 */
+	void (*enable_commit)(struct msm_kms *kms);
+	void (*disable_commit)(struct msm_kms *kms);
+
+	/**
+	 * If the kms backend supports async commit, it should implement
+	 * this method to return the time of the next vsync.  This is
+	 * used to determine a time slightly before vsync, for the async
+	 * commit timer to run and complete an async commit.
+	 */
+	ktime_t (*vsync_time)(struct msm_kms *kms, struct drm_crtc *crtc);
+
+	/**
+	 * Prepare for atomic commit.  This is called after any previous
+	 * (async or otherwise) commit has completed.
+	 */
+	void (*prepare_commit)(struct msm_kms *kms, struct drm_atomic_state *state);
+
+	/**
+	 * Flush an atomic commit.  This is called after the hardware
+	 * updates have already been pushed down to effected planes/
+	 * crtcs/encoders/connectors.
+	 */
+	void (*flush_commit)(struct msm_kms *kms, unsigned crtc_mask);
+
+	/**
+	 * Wait for any in-progress flush to complete on the specified
+	 * crtcs.  This should not block if there is no in-progress
+	 * commit (ie. don't just wait for a vblank), as it will also
+	 * be called before ->prepare_commit() to ensure any potential
+	 * "async" commit has completed.
+	 */
+	void (*wait_flush)(struct msm_kms *kms, unsigned crtc_mask);
+
+	/**
+	 * Clean up after commit is completed.  This is called after
+	 * ->wait_flush(), to give the backend a chance to do any
+	 * post-commit cleanup.
+	 */
+	void (*complete_commit)(struct msm_kms *kms, unsigned crtc_mask);
+
+	/*
+	 * Format handling:
+	 */
+
 	/* get msm_format w/ optional format modifiers from drm_mode_fb_cmd2 */
 	const struct msm_format *(*get_format)(struct msm_kms *kms,
 					const uint32_t format,
-					const uint64_t *modifiers,
-					const uint32_t modifiers_len);
+					const uint64_t modifiers);
 	/* do format checking on format modified through fb_cmd2 modifiers */
 	int (*check_modified_format)(const struct msm_kms *kms,
 			const struct msm_format *msm_fmt,
 			const struct drm_mode_fb_cmd2 *cmd,
 			struct drm_gem_object **bos);
-	/* perform complete atomic check of given atomic state */
-	int (*atomic_check)(struct msm_kms *kms,
-			struct drm_atomic_state *state);
+
 	/* misc: */
 	long (*round_pixclk)(struct msm_kms *kms, unsigned long rate,
 			struct drm_encoder *encoder);
@@ -91,103 +117,81 @@ struct msm_kms_funcs {
 			struct drm_encoder *encoder,
 			struct drm_encoder *slave_encoder,
 			bool is_cmd_mode);
-	void (*postopen)(struct msm_kms *kms, struct drm_file *file);
-	void (*preclose)(struct msm_kms *kms, struct drm_file *file);
-	void (*postclose)(struct msm_kms *kms, struct drm_file *file);
-	void (*lastclose)(struct msm_kms *kms);
-	int (*register_events)(struct msm_kms *kms,
-			struct drm_mode_object *obj, u32 event, bool en);
-	/* pm suspend/resume hooks */
-	int (*pm_suspend)(struct device *dev);
-	int (*pm_resume)(struct device *dev);
+	void (*set_encoder_mode)(struct msm_kms *kms,
+				 struct drm_encoder *encoder,
+				 bool cmd_mode);
 	/* cleanup: */
 	void (*destroy)(struct msm_kms *kms);
-	/* get address space */
-	struct msm_gem_address_space *(*get_address_space)(
-			struct msm_kms *kms,
-			unsigned int domain);
-	/* handle continuous splash  */
-	int (*cont_splash_config)(struct msm_kms *kms);
-	/* check for continuous splash status */
-	bool (*check_for_splash)(struct msm_kms *kms);
+#ifdef CONFIG_DEBUG_FS
+	/* debugfs: */
+	int (*debugfs_init)(struct msm_kms *kms, struct drm_minor *minor);
+#endif
+};
+
+struct msm_kms;
+
+/*
+ * A per-crtc timer for pending async atomic flushes.  Scheduled to expire
+ * shortly before vblank to flush pending async updates.
+ */
+struct msm_pending_timer {
+	struct hrtimer timer;
+	struct work_struct work;
+	struct msm_kms *kms;
+	unsigned crtc_idx;
 };
 
 struct msm_kms {
 	const struct msm_kms_funcs *funcs;
+	struct drm_device *dev;
 
 	/* irq number to be passed on to drm_irq_install */
 	int irq;
+
+	/* mapper-id used to request GEM buffer mapped for scanout: */
+	struct msm_gem_address_space *aspace;
+
+	/*
+	 * For async commit, where ->flush_commit() and later happens
+	 * from the crtc's pending_timer close to end of the frame:
+	 */
+	struct mutex commit_lock;
+	unsigned pending_crtc_mask;
+	struct msm_pending_timer pending_timers[MAX_CRTCS];
 };
 
 static inline void msm_kms_init(struct msm_kms *kms,
 		const struct msm_kms_funcs *funcs)
 {
+	unsigned i;
+
+	mutex_init(&kms->commit_lock);
 	kms->funcs = funcs;
+
+	for (i = 0; i < ARRAY_SIZE(kms->pending_timers); i++)
+		msm_atomic_init_pending_timer(&kms->pending_timers[i], kms, i);
 }
 
-#ifdef CONFIG_DRM_MSM_MDP4
 struct msm_kms *mdp4_kms_init(struct drm_device *dev);
-#else
-static inline
-struct msm_kms *mdp4_kms_init(struct drm_device *dev) { return NULL; };
-#endif
-
-#ifdef CONFIG_DRM_MSM_MDP5
-int msm_mdss_init(struct drm_device *dev);
-void msm_mdss_destroy(struct drm_device *dev);
 struct msm_kms *mdp5_kms_init(struct drm_device *dev);
-#else
-static inline int msm_mdss_init(struct drm_device *dev)
-{
-	return 0;
-}
-static inline void msm_mdss_destroy(struct drm_device *dev)
-{
-}
-static inline struct msm_kms *mdp5_kms_init(struct drm_device *dev)
-{
-	return NULL;
-}
-#endif
-struct msm_kms *sde_kms_init(struct drm_device *dev);
+struct msm_kms *dpu_kms_init(struct drm_device *dev);
 
-/**
- * Mode Set Utility Functions
- */
-static inline bool msm_is_mode_seamless(const struct drm_display_mode *mode)
-{
-	return (mode->flags & DRM_MODE_FLAG_SEAMLESS);
-}
+struct msm_mdss_funcs {
+	int (*enable)(struct msm_mdss *mdss);
+	int (*disable)(struct msm_mdss *mdss);
+	void (*destroy)(struct drm_device *dev);
+};
 
-static inline bool msm_is_mode_seamless_dms(const struct drm_display_mode *mode)
-{
-	return mode ? (mode->private_flags & MSM_MODE_FLAG_SEAMLESS_DMS)
-		: false;
-}
+struct msm_mdss {
+	struct drm_device *dev;
+	const struct msm_mdss_funcs *funcs;
+};
 
-static inline bool msm_is_mode_dynamic_fps(const struct drm_display_mode *mode)
-{
-	return ((mode->flags & DRM_MODE_FLAG_SEAMLESS) &&
-		(mode->private_flags & MSM_MODE_FLAG_SEAMLESS_DYNAMIC_FPS));
-}
+int mdp5_mdss_init(struct drm_device *dev);
+int dpu_mdss_init(struct drm_device *dev);
 
-static inline bool msm_is_mode_seamless_vrr(const struct drm_display_mode *mode)
-{
-	return mode ? (mode->private_flags & MSM_MODE_FLAG_SEAMLESS_VRR)
-		: false;
-}
-
-static inline bool msm_is_mode_seamless_dyn_clk(
-					const struct drm_display_mode *mode)
-{
-	return mode ? (mode->private_flags & MSM_MODE_FLAG_SEAMLESS_DYN_CLK)
-		: false;
-}
-
-static inline bool msm_needs_vblank_pre_modeset(
-		const struct drm_display_mode *mode)
-{
-	return (mode->private_flags & MSM_MODE_FLAG_VBLANK_PRE_MODESET);
-}
+#define for_each_crtc_mask(dev, crtc, crtc_mask) \
+	drm_for_each_crtc(crtc, dev) \
+		for_each_if (drm_crtc_mask(crtc) & (crtc_mask))
 
 #endif /* __MSM_KMS_H__ */
